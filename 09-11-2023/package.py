e06846f3b25f885879f6fad370e2674b190db22f
Binshad <145667652+Bin5had@users.noreply.github.com>
2023-11-09 16:30:10 +0530

    Add files via upload

---
+++ 09-11-2023/package.py
@@ -0,0 +1,10 @@
+from mypackages import summodule as sum1
+from mypackages import avgmodule as avg1
+from mypackages import powmodule as pow1
+
+b = sum1.sum(10,20)
+c = avg1.average(10,20)
+d = pow1.pow(2,3)
+print(b)
+print(c)
+print(d)
